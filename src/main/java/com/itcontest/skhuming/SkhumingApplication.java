d1c69747fc8984108c68396cbf73f0476094b5cb
giwoong <dev.choi0409@gmail.com>
2023-07-09 22:22:48 +0900

    first commit

---
+++ src/main/java/com/itcontest/skhuming/SkhumingApplication.java
@@ -0,0 +1,13 @@
+package com.itcontest.skhuming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkhumingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkhumingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itcontest/skhuming/SkhumingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itcontest.skhuming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkhumingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
