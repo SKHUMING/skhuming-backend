896787fab7ef046bbaaf485e97e916f411f90ee8
giwoong <dev.choi0409@gmail.com>
2023-09-02 23:32:53 +0900

    페이징 기능 구현

--- src/main/java/com/itcontest/skhuming/notice/domain/Notice.java
+++ src/main/java/com/itcontest/skhuming/notice/domain/Notice.java
@@ -2,18 +2,22 @@ package com.itcontest.skhuming.notice.domain;
 
 import com.itcontest.skhuming.member.domain.Member;
 import com.itcontest.skhuming.member.domain.MemberScrapNotice;
+import com.itcontest.skhuming.notice.exception.NotScheduleFormatException;
 import lombok.*;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "Notice")
 @Getter
 public class Notice {
+
+    private static final Pattern SCHEDULE_PATTERN = Pattern.compile("\\d{4}-\\d{1,2}-\\d{1,2}~\\d{4}-\\d{1,2}-\\d{1,2}");
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,8 +40,19 @@ public class Notice {
     }
 
     public Notice(String title, String schedule, String contents, int mileageScore) {
+        notScheduleFormat(schedule);
+
         this.title = title;
-        this.schedule = Objects.requireNonNullElse(schedule, "1/1");
+        this.schedule = schedule;
+        this.contents = contents;
+        this.mileageScore = mileageScore;
+    }
+
+    public void update(String title, String schedule, String contents, int mileageScore) {
+        notScheduleFormat(schedule);
+
+        this.title = title;
+        this.schedule = schedule;
         this.contents = contents;
         this.mileageScore = mileageScore;
     }
@@ -48,11 +63,11 @@ public class Notice {
                 .collect(Collectors.toList());
     }
 
-    public void update(String title, String schedule, String contents, int mileageScore) {
-        this.title = title;
-        this.schedule = schedule;
-        this.contents = contents;
-        this.mileageScore = mileageScore;
+    public void notScheduleFormat(String schedule) {
+        Matcher matcher = SCHEDULE_PATTERN.matcher(schedule);
+        if (!matcher.matches()) {
+            throw new NotScheduleFormatException();
+        }
     }
 
 }
--- src/main/java/com/itcontest/skhuming/ranking/api/RankingController.java
+++ src/main/java/com/itcontest/skhuming/ranking/api/RankingController.java
@@ -2,12 +2,12 @@ package com.itcontest.skhuming.ranking.api;
 
 import com.itcontest.skhuming.member.api.dto.response.MemberRankResDto;
 import com.itcontest.skhuming.ranking.application.RankingService;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RankingController {
@@ -19,7 +19,8 @@ public class RankingController {
     }
 
     @GetMapping("/api/ranking/list")
-    public ResponseEntity<List<MemberRankResDto>> memberRankingList() {
-        return new ResponseEntity<>(rankingService.memberRanking(), HttpStatus.OK);
+    public ResponseEntity<Page<MemberRankResDto>> memberRankingList(@RequestParam(value = "page", defaultValue = "0") int page,
+                                                                    @RequestParam(value = "size", defaultValue = "10") int size) {
+        return new ResponseEntity<>(rankingService.memberRanking(page, size), HttpStatus.OK);
     }
 }
